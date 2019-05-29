b176ef4af4ac9b084c1b85ab2e40c69736e5b3e7
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-29 17:20:24 -0600

    js works

--- main.js
+++ main.js
@@ -12,8 +12,14 @@ var s7 = document.getElementById("7");
 var s8 = document.getElementById("8");
 var s9 = document.getElementById("9");
 var winner = document.getElementById("won")
-var c1="1",c2="c2",c3="c3",c4="c4",c5="c5",c6="c6",c7="c7",c8="c8",c9="c9";
+var c1="c1",c2="c2",c3="c3",c4="c4",c5="c5",c6="c6",c7="c7",c8="c8",c9="c9";
+const c_array = [c1,c2,c3,c4,c5,c6,c7,c8,c9]
 
+// for (var i = 0; i < c_array.length; i++){
+//   if (i == pick){
+//     c_array[i] = "x"
+//   }
+// }
 //const moves = [s1,s2,s3,s4,s5,s6,s7,s8,s9]
 //const moves = [s1,s2,s3,s4,s5,s6,s7,s8,s9]
 
@@ -23,13 +29,33 @@ function display_input(choice) {
     let content = document.createElement("h1");
     content.className = "user";
     content.textContent = "x";
+    // console.log(choice.id)
     choice.appendChild(content);
-    // console.log(player)
+    var pick = ("c" + choice.id)
+    c_array.forEach((item)=>{
+      if (item == pick){
+        c_array[choice.id-1] = "x"
+        console.log(c_array)
+        console.log(c4)
+        // console.log(c_array)
+      }
+    })
+    //console.log(player)
     player = 0;
   } else {
     let content = document.createElement("h1");
     content.className = "user";
     content.textContent = "o";
+    // console.log(choice.id)
+    var pick = ("c" + choice.id)
+    // console.log(pick)
+    c_array.forEach((item)=>{
+      if (item == pick){
+        c_array[choice.id-1] = "o"
+        console.log(c_array)
+        // console.log(c_array)
+      }
+    })
     choice.appendChild(content);
     player = 1;
   }
@@ -42,7 +68,7 @@ for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener("click", function() {
     userChoice = this.id;
 
-     console.log(player)
+    //  console.log(player)
     // content = document.createElement("h1");
     // content.className = "user";
     // content.textContent = userChoice
@@ -86,7 +112,8 @@ for (var i = 0; i < squares.length; i++) {
         display_input(s9);
         break;
     }
-    var trios = [[c1,c2,c3],[c4,c5,c6],[c7,c8,c9],[c1,c4,c7],[c2,c5,c8],[c3,c6,c9],[c1,c5,c9],[c3,c5,c7]];
+    // var trios = [[c1,c2,c3],[c4,c5,c6],[c7,c8,c9],[c1,c4,c7],[c2,c5,c8],[c3,c6,c9],[c1,c5,c9],[c3,c5,c7]];
+    var trios = [[c_array[1-1],c_array[2-1],c_array[3-1]],[c_array[4-1],c_array[5-1],c_array[6-1]],[c_array[7-1],c_array[8-1],c_array[9-1]],[c_array[1-1],c_array[4-1],c_array[7-1]],[c_array[2-1],c_array[5-1],c_array[8-1]],[c_array[3-1],c_array[6-1],c_array[9-1]],[c_array[1-1],c_array[5-1],c_array[9-1]],[c_array[3-1],c_array[5-1],c_array[7-1]]];
     trios.forEach( function(element) {
       if (element.every( (val, i, arr) => val === "x" ) === true) {
         console.log("player 1 wins")
