afb44eb1fd1d7c9ba10bcb7c3a51d58e0dc0dbf4
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-29 13:35:09 -0600

    initialized

---
+++ challenge_1.js
@@ -0,0 +1,83 @@
+var alphas = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var word = "hellabco"
+
+function alphaOrder(word){
+  newArray = []
+  var arr = word.split
+  var alphas = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  for (var i= 0; i <= alphas.length -1; i++){
+    for (var j = 0; j <= word.length; j++){
+      if (alphas[i] === arr[j]){
+        newArray.push(arr[j])
+      }
+    }
+  }
+  
+return newArray
+}
+alphaOrder(word)
+
+alphas.forEach((x){
+
+})
+function luckySeven(){
+
+
+  arr = [1,2,5]
+  
+  arr.forEach((num) => {
+    if ((num) + (num++ ) + (num+2) == 7){
+      console.log(true)
+    }
+  })
+}
+
+for (var i = 0; i > arr.length; i++){
+  var value = (arr[i] + arr[i++])
+
+}
+
+
+
+
+ arr = [1,2,5,6,3,4,2,1,2,3,4,5,67,8,1,2,4,5]
+
+
+
+function findValue(arr){
+ for (var i = 0; i < arr.length-1; i++){
+ num = ((arr[i]) + (arr[i+1])+ (arr[i+2]))
+  if (num == 7){
+    return true
+  }
+}
+}
+
+
+
+
+
+
+
+function luckySevens(b){
+for(var i = 0; i < b.length-1; ++i) {
+add_numbers = ((b[i]) + (b[i+1]) + (b[i+2]))
+if (add_numbers == 7) {
+return true
+}
+}
+// console.log(findValue(arr))
+
+c = [1,1,5,7,4,2,3,4,5,6,8,7,3,7,2,1,7,5,1,6,1,5,2]
+b = [8,7,6,7,8,5,4,5,8,7,8,2,4,1]
+a = [1,3,4,6,8,3,5,2,8,9,3,6]
+
+console.log(luckySevens(b))
+
+
+function game(){
+  //define the array 
+  var arr = [1,2,3,4,5,6,7,8,9]
+  let p1_image = document.createElement
+
+}
---
+++ main.js
@@ -0,0 +1,70 @@
+var squares = document.getElementsByClassName("game_square");
+var userChoice;
+console.log(squares);
+// var test = document.getElementById("1")
+var s1 = document.getElementById("1")
+var s2 = document.getElementById("2")
+var s3 = document.getElementById("3")
+var s4 = document.getElementById("4")
+var s5 = document.getElementById("5")
+var s6 = document.getElementById("6")
+var s7 = document.getElementById("7")
+var s8 = document.getElementById("8")
+var s9 = document.getElementById("9")
+const moves = [s1,s2,s3,s4,s5,s6,s7,s8,s9]
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener("click", function() {
+    userChoice = this.id;
+    console.log(userChoice);
+    console.log(`s${userChoice}`);
+    let x_content = document.createElement("h1");
+    x_content.className = "user";
+    x_content.textContent = "X";
+    switch (userChoice) {
+      case "1":
+        removeContent();
+        s1.appendChild(x_content);
+        break;
+      case "2":
+        removeContent();
+        s2.appendChild(x_content);
+        break;
+      case "3":
+        removeContent();
+        s3.appendChild(x_content);
+        break;
+      case "4":
+        removeContent();
+        s4.appendChild(x_content);
+        break;
+      case "5":
+        removeContent();
+        s5.appendChild(x_content);
+        break;
+      case "6":
+        removeContent();
+        s6.appendChild(x_content);
+        break;
+      case "7":
+        removeContent();
+        s7.appendChild(x_content);
+        break;
+      case "8":
+        removeContent();
+        s8.appendChild(x_content);
+        break;
+      case "9":
+        removeContent();
+        s9.appendChild(x_content);
+        break;
+    }
+  });
+}
+
+const removeContent = () => {
+  let removeH1 = document.getElementById(userChoice.toString());
+  while (removeH1.firstChild) {
+    removeH1.removeChild(removeH1.firstChild);
+  }
+};
